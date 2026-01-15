cfd37096c68485af8fa5fc94a41a5db87c7580d2
Marcos Vogado <marcoshcvogado@gmail.com>
2026-01-15 14:29:09 -0300

    Refactor banda evaluation to separate menu class
    Moved the logic for evaluating a band from Program.cs into a new MenuAvaliarBanda class for better separation of concerns. Updated Program.cs to use the new menu class and removed the old AvaliarUmaBanda method. Cleaned up csproj folder includes.

--- ScreenSound03-projeto-inicial/ScreenSound/Program.cs
+++ ScreenSound03-projeto-inicial/ScreenSound/Program.cs
@@ -54,11 +54,13 @@ void ExibirOpcoesDoMenu()
             MostrarBandasRegistradas();
             break;
         case 4:
-            AvaliarUmaBanda();
+            MenuAvaliarBanda menuBandasRegistradas = new MenuAvaliarBanda();
+            menuBandasRegistradas.Executar(bandasRegistradas);
+            ExibirOpcoesDoMenu();
             break;
         case 5:
-            MenuExibirDetalhes menu = new MenuExibirDetalhes();
-            menu.Executar(bandasRegistradas);
+            MenuExibirDetalhes menuDetalhes = new MenuExibirDetalhes();
+            menuDetalhes.Executar(bandasRegistradas);
             ExibirOpcoesDoMenu();
             break;
         case -1:
@@ -138,34 +140,6 @@ void ExibirTituloDaOpcao(string titulo)
     Console.WriteLine(asteriscos);
     Console.WriteLine(titulo);
     Console.WriteLine(asteriscos + "\n");
-}
-
-void AvaliarUmaBanda()
-{
-    Console.Clear();
-    ExibirTituloDaOpcao("Avaliar banda");
-    Console.Write("Digite o nome da banda que deseja avaliar: ");
-    string nomeDaBanda = Console.ReadLine()!;
-    if (bandasRegistradas.ContainsKey(nomeDaBanda))
-    {
-        Banda banda = bandasRegistradas[nomeDaBanda];
-        Console.Write($"Qual a nota que a banda {nomeDaBanda} merece: ");
-        Avaliacao nota = Avaliacao.Parse(Console.ReadLine()!);
-        banda.AdicionarNota(nota);
-        Console.WriteLine($"\nA nota {nota.Nota} foi registrada com sucesso para a banda {nomeDaBanda}");
-        Thread.Sleep(2000);
-        Console.Clear();
-        ExibirOpcoesDoMenu();
-    }
-    else
-    {
-        Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada!");
-        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
-        Console.ReadKey();
-        Console.Clear();
-        ExibirOpcoesDoMenu();
-    }
-
 }
 
 ExibirOpcoesDoMenu();
