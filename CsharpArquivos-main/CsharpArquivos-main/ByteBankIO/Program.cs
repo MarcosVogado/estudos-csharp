6ac7c24b561f2825ea73fdd66d466e3234c3c5b0
Marcos Vogado <marcoshcvogado@gmail.com>
2026-02-11 15:21:52 -0300

    add

--- CsharpArquivos-main/CsharpArquivos-main/ByteBankIO/Program.cs
+++ CsharpArquivos-main/CsharpArquivos-main/ByteBankIO/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
+        var enderecoDoArquivo = "contas.txt";
+        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+        var buffer = new byte[1024]; //1Kb
+
+        // public override int Read(byte[] buffer, int offset, int count)
+        fluxoDoArquivo.Read(buffer, 0, 1024);
 
         Console.ReadLine();
     }
