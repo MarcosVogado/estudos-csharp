46ba64de10b030a7f24224d055ce04531e2d4d7a
Marcos Vogado <marcoshcvogado@gmail.com>
2026-02-23 16:45:17 -0300

    Update Program.cs

--- CsharpArquivos-main/CsharpArquivos-main/ByteBankIO/Program.cs
+++ CsharpArquivos-main/CsharpArquivos-main/ByteBankIO/Program.cs
@@ -6,37 +6,41 @@ class Program
     static void Main(string[] args)
     {
         var enderecoDoArquivo = "contas.txt";
-        var numeroDeBytesLidos = -1;
-        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-        var buffer = new byte[1024]; //1Kb
-        
-         Amanhã volto a programar cbls.
-        // public override int Read(byte[] buffer, int offset, int count)
-        numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
 
-        while(numeroDeBytesLidos != 0)
-        {
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open)){
+            var numeroDeBytesLidos = -1;
+
+            var buffer = new byte[1024]; //1Kb
+
+            //Voltei aos estudos fi, agora ninguém mais segura a máquina.
+            // public override int Read(byte[] buffer, int offset, int count)
             numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-            EscreverBuffer(buffer);
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
+            fluxoDoArquivo.Close();
+
+            Console.ReadLine();
         }
 
-        fluxoDoArquivo.Close();
-
-        Console.ReadLine();
-    }
-
-    static void EscreverBuffer(byte[] buffer)
-    {
-        var utf8 = new UTF8Encoding();
-        var texto = utf8.GetString(buffer);
-
-        Console.Write(texto);
-        /*
-        foreach (var meuBite in buffer)
+        static void EscreverBuffer(byte[] buffer)
         {
-            Console.Write(meuBite);
-            Console.Write(" ");
+            var utf8 = new UTF8Encoding();
+            var texto = utf8.GetString(buffer);
+
+            Console.Write(texto);
+            /*
+            foreach (var meuBite in buffer)
+            {
+                Console.Write(meuBite);
+                Console.Write(" ");
+            }
+            */
         }
-        */
     }
+        
 }
