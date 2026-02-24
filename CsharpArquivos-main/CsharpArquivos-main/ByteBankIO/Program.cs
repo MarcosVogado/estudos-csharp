29196329f78c33034eab2cc71bedfda8772a36d7
Marcos Vogado <marcoshcvogado@gmail.com>
2026-02-24 16:06:02 -0300

    Update Program.cs

--- CsharpArquivos-main/CsharpArquivos-main/ByteBankIO/Program.cs
+++ CsharpArquivos-main/CsharpArquivos-main/ByteBankIO/Program.cs
@@ -5,6 +5,24 @@ using System.Text;
 {
     static void Main(string[] args)
     {
+        var enderecoDoArquivo = "contas.txt";
+
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var leitor = new StreamReader(fluxoDoArquivo);
+
+            //var linha = leitor.ReadLine();
+            //var texto = leitor.ReadToEnd();
+            //var numero = leitor.Read();
+
+            while (!leitor.EndOfStream)
+            {
+                var linha = leitor.ReadLine();
+                Console.WriteLine(linha);
+            }
+
+        }
+
         Console.ReadLine();
     }
 }
