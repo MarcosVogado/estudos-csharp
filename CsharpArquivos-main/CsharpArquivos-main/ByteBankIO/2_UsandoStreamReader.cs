c0fb8321fa7d929663a0a4607baa11243cd569d2
Marcos Vogado <marcoshcvogado@gmail.com>
2026-02-26 13:53:21 -0300

    Reorganizing the project

---
+++ CsharpArquivos-main/CsharpArquivos-main/ByteBankIO/2_UsandoStreamReader.cs
@@ -0,0 +1,53 @@
+﻿using ByteBankIO;
+using System.Globalization;
+using System.Text;
+
+partial class Program
+{
+    public void UsandoStreamReader()
+    {
+        var enderecoDoArquivo = "contas.txt";
+
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var leitor = new StreamReader(fluxoDoArquivo);
+
+            //var linha = leitor.ReadLine();
+            //var texto = leitor.ReadToEnd();
+            //var numero = leitor.Read();
+
+            while (!leitor.EndOfStream)
+            {
+                var linha = leitor.ReadLine();
+                var contaCorrente = ConverterStringParaContaCorrente(linha);
+
+                var msg = $"Titular: {contaCorrente.Titular.Nome}, Agência: {contaCorrente.Agencia}, Número: {contaCorrente.Numero}, Saldo: $ {contaCorrente.Saldo}";
+                Console.WriteLine(msg);
+
+            }
+
+        }
+
+        Console.ReadLine();
+
+
+        static ContaCorrente ConverterStringParaContaCorrente(string linha)
+        {
+            var campos = linha.Split(',');
+
+            var agencia = int.Parse(campos[0]);
+            var numero = int.Parse(campos[1]);
+            var saldo = double.Parse(campos[2], CultureInfo.InvariantCulture);
+            var titular = campos[3];
+
+            var titularDaConta = new Cliente();
+            titularDaConta.Nome = titular;
+
+            var conta = new ContaCorrente(agencia, numero);
+            conta.Titular = titularDaConta;
+            conta.Depositar(saldo);
+
+            return conta;
+        }
+    }
+}
