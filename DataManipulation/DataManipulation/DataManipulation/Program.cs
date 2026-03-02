93e1c4413641ca93ecd3d4fd89d364df6d2102a9
Marcos Vogado <marcoshcvogado@gmail.com>
2026-03-02 14:55:44 -0300

    Setup new study project

---
+++ DataManipulation/DataManipulation/DataManipulation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataManipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
