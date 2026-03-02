3bdb8b8ae1a2e18e5302c12f24e71e5b42964eb0
Marcos Vogado <marcoshcvogado@gmail.com>
2026-03-02 15:08:56 -0300

    Update Program.cs

--- DataManipulation/DataManipulation/DataManipulation/Program.cs
+++ DataManipulation/DataManipulation/DataManipulation/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace DataManipulation
+﻿using System.Collections;
+
+namespace DataManipulation
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var diasDaSemana = new string[]
+            { 
+                "Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado" 
+            };
+
+            var carrinho = new ArrayList()
+            {
+                new Produto { Titulo = "Livro", Preco = 29.90 },
+                new Produto { Titulo = "Caderno", Preco = 9.90 },
+                new Produto { Titulo = "Caneta", Preco = 4.90 }
+            };
         }
+    }
+
+    class Produto
+    {
+        public string Titulo { get; set; }
+        public double Preco { get; set; }
+
     }
 }
