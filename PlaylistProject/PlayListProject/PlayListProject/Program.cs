b5c9ef81627d5b4a008570a8d64f78686f34ef17
Marcos Vogado <marcoshcvogado@gmail.com>
2026-03-05 11:56:44 -0300

    Setup

---
+++ PlaylistProject/PlayListProject/PlayListProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PlayListProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
